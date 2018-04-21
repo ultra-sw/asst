f0aea103162cec015f4e32daf591e2c385279efa
borninjuly <borninjuly18th@outlook.com>
2018-04-21 19:47:48 +0300

    Исправление ошибок. Исправлена ошибка при входе, которая перенаправляла на не существующую страницу инф. о пользователе, добавлен driver-class-name (без него проект не запускался), исправлены ссылки и поля.

--- src/main/java/ru/ultrasoftware/asst/security/WebSecurityConfig.java
+++ src/main/java/ru/ultrasoftware/asst/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // Submit URL of login page.
                 .loginProcessingUrl("/j_spring_security_check") // Submit URL
                 .loginPage("/login")//
-                .defaultSuccessUrl("/userAccountInfo")//
+                .defaultSuccessUrl("/user-info")//
                 .failureUrl("/login?error=true")//
                 .usernameParameter("username")//
                 .passwordParameter("password")
